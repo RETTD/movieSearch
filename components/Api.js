114e504949ae11a339f9bf2e9d1730172b5b6f79
RETTD <57707343+RETTD@users.noreply.github.com>
2021-02-08 21:17:44 +0100

    fix request api

--- components/Api.js
+++ components/Api.js
@@ -2,7 +2,7 @@ export const fetchMovies = async (searchMovie, page) => {
   const apiPage = page != undefined ? parseFloat(page) + 1 : 1;
   
   const response = await fetch(
-    "http://www.omdbapi.com/?apikey=8e2acd21&s=" +
+    "https://www.omdbapi.com/?apikey=8e2acd21&s=" +
       searchMovie +
       "&page=" +
       apiPage
